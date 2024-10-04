e88a860f068ec2cb3becc4617c463f1e3b218b03
Mehretab Hailegebriel <cyrmee@gmail.com>
2024-10-04 17:01:40 +0300

    334. Increasing Triplet Subsequence

---
+++ 334.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool increasingTriplet(vector<int> &nums)
+{
+    if (nums.size() < 3)
+        return false;
+
+    int firstValue = INT_MAX,
+        secondValue = INT_MAX,
+        firstIndex = 0,
+        secondIndex = 0,
+        size = nums.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        if (nums[i] <= firstValue)
+        {
+            firstValue = nums[i];
+            firstIndex = i;
+        }
+        else if (nums[i] <= secondValue && i > firstIndex)
+        {
+            secondValue = nums[i];
+            secondIndex = i;
+        }
+        else if (i > secondIndex)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> nums = {20, 100, 10, 12, 5, 13};
+    cout << increasingTriplet(nums) << endl;
+    return 0;
+}
