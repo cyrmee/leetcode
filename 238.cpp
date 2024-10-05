0a84bb56b69436a2b229a019fce17903f5046530
Mehretab Hailegebriel <cyrmee@gmail.com>
2024-10-05 09:09:40 +0300

    238. Product of Array Except Self

---
+++ 238.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    int size = nums.size();
+    vector<int> products(size, 1);
+
+    for (int i = 1; i < size; i++)
+    {
+        products[i] = products[i - 1] * nums[i - 1];
+    }
+
+    int rightProduct = 1;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        products[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return products;
+}
+
+int main()
+{
+    vector<int> nums = {-1, 1, 0, -3, 3};
+    productExceptSelf(nums);
+    return 0;
+}
